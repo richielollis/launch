7efad2f21636103f263aff163b29644b1fdb8336
richielollis <richielollisdesktop@richies-mini.lan>
2022-08-27 13:06:58 -0400

    added error for no name or option selected, touch up a little styling, comlpeted rendered rocket card upon submit, also added go again button for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,20 @@ var getUserName = function() {
     var uName= localStorage.getItem('name');
     if(uName) {
         $('#welcome').text(`Welcome back ${uName}!`)
+        $('#first_name').val(uName);
     } 
-    // grab name from form input once blastoff button is clicked
-   
-    // set name to local storage to be able to be used later and for page reload once user has already entered name
-    
 }
 
 var blastoff = function(event) {
     event.preventDefault();
     
     var name = $('#first_name').val();
-
+    // make sure this if statement check that the user has entered a name and selected an option!!!!!!!
     if(name) {
         console.log(name);
         localStorage.setItem('name', name);
     } else {
-        alert('Must Enter Name!');
+        $('#error-card').show();
         return;
     }
 
@@ -29,25 +26,28 @@ var blastoff = function(event) {
     $('#info-card').show();
 };
 
+var closeError = function(event) {
+    event.preventDefault();
+    $('#error-card').css('display', 'none');
+}
+
 var getSpaceInfo  = function() {
     fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${today}&end_date=${today}&api_key=8mtv2mUNhRq4AdcG3eLJYaGFiJyctDairkAmuZoa`)
     .then(function(res) {
-    //  console.log(res)
         return res.json();
     }) 
     .then(nasaInfo => {
     asteroids(nasaInfo)
-    console.log(nasaInfo)
     })
-    
-    fetch('https://api.spacexdata.com/v4/rockets')
-    .then(function(res) {
-        // console.log(res)
-        return res.json();
-    }) 
-    .then(spacexInfo => {
-        showUserRocket(spacexInfo);
-        console.log(spacexInfo);
+    .then (function(){
+        fetch('https://api.spacexdata.com/v4/rockets')
+        .then(function(res) {
+            return res.json();
+        }) 
+        .then(spacexInfo => {
+            showUserRocket(spacexInfo);
+            console.log(spacexInfo);
+        })
     })
     .catch(function(error) {
          console.log(error);
@@ -67,39 +67,86 @@ var asteroids = function(nasaInfo) {
 var showUserRocket = function(spacexInfo) {
     // create if statement to check which option the user clicked and which rocket to render
     var trait = document.getElementById('trait-input');
-    
+    var rocketNumber;
     console.log(trait);
     if (trait === 'something'){
-        var rocketNumber = spacexInfo[1];
     }
+    rocketNumber = spacexInfo[2];
 
-    $('#rocket-name').text(spacexInfo[1].name);
+    $('#rocket-name').text(rocketNumber.name);
+
     var rocket = $('#rocket');
+    
+    rocket.attr('src', rocketNumber.flickr_images[3]);
+    rocket.css('border-radius', '5px');
+    rocket.css('max-width', '30vw');
+    rocket.css('min-width', '300px');
+    rocket.css('margin', '20px');
+    
     var rocketInfo = $('#rocket-info');
 
-    rocket.attr('src', spacexInfo[1].flickr_images[3]);
-    rocket.css('border-radius', '5px');
-    rocket.css('width', '30vw');
-    rocket.css('margin', '20px');
+    var infoDiv = $('<div></div>');
+    infoDiv.attr('class', 'row');
 
-    var info = $('<h6></h6>');
-    if (spacexInfo[1].stages > 1) {
-        info.text(`${spacexInfo[1].name} is a ${spacexInfo[1].stages} stage rocket! In Stage 1 there are ${spacexInfo[1].first_stage.engines}. In Stage 2 there `);
-    } else {
+    var infoListDiv = $('<div></div>');
+    infoListDiv.attr('class', 'col s4 right-align');
+  
+    var infoListDiv2 = $('<div></div>');
+    infoListDiv2.attr('class', 'offset-s4 col s4 left-align');
+  
 
-    }
+    var infoList = $('<ul></ul>');
+    infoList.css('list-decoration', 'none');
 
-    rocketInfo.append(info);
+    var engines = $('<li></li>');
+    engines.text(`Engines: ${rocketNumber.engines.number}`);
+    infoList.append(engines);
+
+    var stages = $('<li></li>');
+    stages.text(`Stages: ${rocketNumber.stages}`);
+    infoList.append(stages);
+
+    var mass = $('<li></li>');
+    var num = rocketNumber.mass.lb;
+    mass.text(`Mass: ${num} lbs.`);
+    infoList.append(mass);
+
+    var infoList2 = $('<ul></ul>');
+    infoList2.css('list-decoration', 'none');
+
+    var diameter = $('<li></li>');
+    diameter.text(`Diameter: ${rocketNumber.diameter.feet} ft.`);
+    infoList2.append(diameter);
+
+    var height = $('<li></li>');
+    height.text(`Height: ${rocketNumber.height.feet} ft.`);
+    infoList2.append(height);
+
+    var successRate = $('<li></li>');
+    successRate.text(`Success Rate: ${rocketNumber.success_rate_pct} %`);
+    infoList2.append(successRate);
+
     
 
+    infoListDiv.append(infoList);
+
+    infoListDiv2.append(infoList2);
+
+    infoDiv.append(infoListDiv);
+    infoDiv.append(infoListDiv2);
+
+    rocketInfo.append(infoDiv);
+    
 
 };
 
-// create blastoff button eent listener 
-// have a back button to go select new rocket --- make sure user name will show when going back to select new rocket
+var goAgain = function(event) {
+    event.preventDefault();
 
+}
 
 //https://api.nasa.gov/insight_weather/?api_key=8mtv2mUNhRq4AdcG3eLJYaGFiJyctDairkAmuZoa&feedtype=json&ver=1.0
 
 $('#findbtn').on('click', blastoff);
+$('#close-error').on('click', closeError);
 getUserName();
